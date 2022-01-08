d419072cd3f2983890720e67f6ed36cb474aa650
ryanopoku <ryanopoku@gmail.com>
2022-01-08 21:43:04 +0000

    Add files via upload

---
+++ plurality.c
@@ -0,0 +1,104 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Maximum number of candidates (9)
+#define MAX 9
+
+
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// candidates list
+candidate candidates[MAX];
+
+// candidates number
+int candidate_count;
+
+
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("The maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Displays winner
+    print_winner();
+}
+
+
+bool vote(string name)
+{
+    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    // value is false and loops till true
+    return false;
+}
+
+// Print the winner (or winners if there is multiple)
+void print_winner(void)
+{
+    int maxvotes = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maxvotes)
+        {
+            maxvotes = candidates[i].votes;
+        }
+    }
+    // prints out the winners
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == maxvotes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+
+
+    return;
+}
